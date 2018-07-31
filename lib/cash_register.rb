370aab4e1f6da8d6f4d65296c8035e74d2de6e82
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-31 22:52:15 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,7 @@ class CashRegister
   def apply_discount
     if @discount != 0
     total_w_discount = @total - @discount
+    total_w_discount
     "#{total_w_discount}"
     else
       "There is no discount to apply."
